2e4399aba1b9d20127e200b84f8ea3f1a4f7530d
YYJ0311 <dudwns0311@naver.com>
2022-10-27 23:59:40 +0900

    fix : 1027 python

--- PythonWorkspace/gui/2. gui_project/1_create_layout.py
+++ PythonWorkspace/gui/2. gui_project/1_create_layout.py
@@ -1,10 +1,97 @@
+import tkinter.ttk as ttk
 from tkinter import *
 
 root = Tk()
-root.title("Start GUI") # 프로그램 이름
-root.geometry("640x480") # 가로x세로 크기지정
-# root.geometry("640x480+300+300") # 프로그램 뜨는 위치 지정. + x좌표 + y좌표
+root.title("gogo gui")
 
-root.resizable(False, False) # x(너비), y(높이) 값 변경 불가. 창 크기 변경 불가
+# 파일 프레임 (파일 추가, 선택 삭제)
+file_frame = Frame(root)
+file_frame.pack(fill="x") # 가로 확장
 
-root.mainloop() # 창이 닫히지 않게 만듦
+btn_add_file = Button(file_frame, text="파일추가", padx=5, pady=5, width=12)
+btn_add_file.pack(side="left")
+
+btn_del_file = Button(file_frame, text="선택삭제", padx=5, pady=5, width=12)
+btn_del_file.pack(side="right")
+
+# 리스트 프레임
+list_frame = Frame(root)
+list_frame.pack(fill="both")
+
+scrollbar = Scrollbar(list_frame)
+scrollbar.pack(side="right", fill="y")
+
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+list_file.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=list_file.yview)
+
+# 저장 경로 프레임
+path_frame = LabelFrame(root, text="저장경로")
+path_frame.pack(fill="x")
+
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # ipad : 안쪽 패딩 값 -> 높이 변경
+
+btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path.pack(side="right")
+
+# 옵션 프레임
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack()
+
+# 1. 가로 넓이 옵션
+# 가로 넓이 레이블
+lbl_width = Label(frame_option, text="가로넓이", width=8)
+lbl_width.pack(side="left")
+
+# 가로 넓이 콤보
+opt_width = ["원본유지", "1024", "800", "640"]
+cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
+cmb_width.current(0) # 0번째 인덱스를 디폴트로
+cmb_width.pack(side="left")
+
+
+# 2. 간격 옵션
+# 간격 옵션 레이블
+lbl_space = Label(frame_option, text="간격", width=8)
+lbl_space.pack(side="left")
+
+# 간격 옵션 콤보
+opt_space = ["없음", "좁게", "보통", "넓게"]
+cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
+cmb_space.current(0) # 0번째 인덱스를 디폴트로
+cmb_space.pack(side="left")
+
+
+# 3. 파일 포맷 옵션
+# 파일 포맷 옵션 레이블
+lbl_format = Label(frame_option, text="포맷", width=8)
+lbl_format.pack(side="left")
+
+# 파일 포맷 옵션 콤보
+opt_format = ["PNG", "JPG", "BMP"]
+cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
+cmb_format.current(0) # 0번째 인덱스를 디폴트로
+cmb_format.pack(side="left")
+
+
+# 진행 상황 Progess bar
+frame_progress = LabelFrame(root, text="진행상황")
+frame_progress.pack(fill="x")
+
+p_var = DoubleVar()
+progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
+progress_bar.pack(fill="x")
+
+# 실행 프레임
+frame_run = Frame(root)
+frame_run.pack(fill="x")
+
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start.pack(side="right")
+
+btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
+btn_close.pack(side="right")
+
+root.resizable(False, False)
+root.mainloop()
