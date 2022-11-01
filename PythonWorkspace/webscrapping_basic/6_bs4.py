fad7e78bd8550ed7a28c246e2250acf5fa01a7f9
YYJ0311 <dudwns0311@naver.com>
2022-11-01 23:43:11 +0900

    fix : 1101 python

--- PythonWorkspace/webscrapping_basic/6_bs4.py
+++ PythonWorkspace/webscrapping_basic/6_bs4.py
@@ -23,3 +23,26 @@ print(soup.find("li", attrs={"class":"rank01"}))
 rank1 = soup.find("li", attrs={"class":"rank01"})
 print(rank1.a) # <a href="/webtoon/detail?titleId=783053&amp;no=53" onclick="nclk_v2(event,'rnk*p.cont','783053','1')" title="김부장-53화 그게 뭔지 아세요?">김부장-53화 그게 뭔지 아세요?</a>
 # soup을 통해 발견한 객체를 다시 soup 객체와 같이 다룸. a태그만 가져옴
+
+print(rank1.a.get_text()) # 1위 웹툰 text만
+print(rank1.next_sibling) # 아무것도 안 나옴. 순위 사이에 줄바꿈 때문
+print(rank1.next_sibling.next_sibling) # 2위 웹툰 정보 출력
+
+rank2 = rank1.next_sibling.next_sibling
+rank3 = rank2.next_sibling.next_sibling
+print(rank3.a.get_text())
+rank2 = rank3.previous_sibling.previous_sibling # previous로 이전 항목 가져올 수 있음
+print(rank2.a.get_text())
+
+print(rank1.parent) # ol 태그 출력됨
+rank2 = rank1.find_next_sibling("li") # 조건(li)에 해당하는 것만 찾음
+print(rank2.a.get_text()) # 중간의 줄바꿈 신경쓰지 않고 바로 출력할 수 있음
+rank3 = rank2.find_next_sibling("li")
+print(rank3.a.get_text())
+rank2 = rank3.find_previous_sibling("li")
+print(rank2.a.get_text())
+
+print(rank1.find_next_siblings("li")) # 다음 li들을 모두 가져옴
+
+webtoon = soup.find("a", text="내 남편과 결혼해줘-53화") # text에 해당하는 a태그를 가져옴
+print(webtoon)
